7105840d9424e4ac2f50db148da35ec3d673a291
joelcre <joel_creutzberg@outlook.com>
2021-02-24 16:13:59 +0100

    Adding function to construct spectra for dalton files

--- construct_cpp_spectra.py
+++ construct_cpp_spectra.py
@@ -16,54 +16,92 @@ def clean_list(old_list):
     for element in old_list:
         if len(element)>0:
             new_list.append(element)
-    
-
     return new_list
 
 
 #----------------Function to extract polarizabilites from file and construct a spectra-------------#
-def construct_spectrum(f):
+def construct_spectrum_dalton(f):
+    PI=math.pi
+    C= 137.036 #Speed of light in atomic units
+    file=open(f,"r")
+    lines=[]
+    X_pol=[]
+    Y_pol=[]
+    Z_pol=[]
+
+    for line in file:
+        if  'XDIPLEN   XDIPLEN' in line:  
+            X_pol.append(line.split(' '))
+        if  'YDIPLEN   YDIPLEN' in line:  
+            Y_pol.append(line.split(' '))
+        if  'ZDIPLEN   ZDIPLEN' in line:  
+            Z_pol.append(line.split(' '))
+
+    for i in range(0,len(X_pol)):
+        X_pol[i]=clean_list(X_pol[i])
+        Y_pol[i]=clean_list(Y_pol[i])
+        Z_pol[i]=clean_list(Z_pol[i])
+
+    spectrum=np.zeros((len(X_pol),2))
+    for j in range(0,len(X_pol)):
+        spectrum[j,0]=float(X_pol[j][3]) #energy
+        spectrum[j,1]= ((4*PI*spectrum[j,0])/C)*(1.0/3.0)*(float(X_pol[j][5])+float(Y_pol[j][5])+float(Z_pol[j][5])) #
+     
+    file.close
+    return spectrum
+    
+def construct_spectrum_dirac(f,nr_freq):
     PI=math.pi
     C= 137.036 #Speed of light in atomic units
     
     file=open(f,"r")
-    xx=[]
-    yy=[]
-    zz=[]
-    freq=[]
-    line_ptr=False
-
+    T_im=np.zeros((nr_freq,3)) #imaginary part of polarizability tensor
+    freq=np.zeros((3))
+    line_ptr=0
+    lines=file.readlines()
     #extract trace of the electric dipole polarizability tensor
-    for line in file:
-        if '<< 1, 1>>:' in line:
-            xx.append((clean_list(line.split(' ')))[3])
-            freq.append((clean_list(line.split(' ')))[8])
-        if '<< 2, 2>>:' in line:
-            yy.append((clean_list(line.split(' ')))[3])
-        if '<< 3, 3>>:' in line:
-            zz.append((clean_list(line.split(' ')))[3])
+    for line in lines:
+        if '<<A( 1),B( 1)>>' in line:
+            temp_lines=lines[line_ptr+7:line_ptr+7+nr_freq]  
+            for i in range(0,len(temp_lines)):
+                temp_lines[i]=clean_list(temp_lines[i].split(' '))
+                freq[i]=float(temp_lines[i][0])
+                T_im[i,0]=float(temp_lines[i][4])
+                
+        if '<<A( 2),B( 2)>>' in line:
+            temp_lines=lines[line_ptr+7:line_ptr+7+nr_freq]  
+            for i in range(0,len(temp_lines)):
+                temp_lines[i]=clean_list(temp_lines[i].split(' '))
+                T_im[i,1]=float(temp_lines[i][4])
+                
+        if '<<A( 3),B( 3)>>' in line:
+            temp_lines=lines[line_ptr+7:line_ptr+7+nr_freq]  
+            for i in range(0,len(temp_lines)):
+                temp_lines[i]=clean_list(temp_lines[i].split(' '))
+                T_im[i,2]=float(temp_lines[i][4])
+           
+        line_ptr=line_ptr+1
 
-    # take out the imaginary/real part of the traces of the electric dipole polarizability tensor      
-    xx=xx[1:len(xx):2]  # real if from 0, from 1 imaginary 
-    yy=yy[1:len(yy):2]
-    zz=zz[1:len(zz):2]
-    freq=freq[0:len(freq):2]
+    
+
     file.close
 
-    #
+    
     spectrum=np.zeros((len(freq),2))
     for i in range(0,len(freq)):
-        spectrum[i,0]=float(freq[i])
-        spectrum[i,1]=-((4*PI*spectrum[i,0])/C)*((1/3)*(float(xx[i])+float(yy[i])+float(zz[i]))) #convert to cross-section
-        
+        spectrum[i,0]=freq[i]
+        spectrum[i,1]=-((4*PI*spectrum[i,0])/C)*((1/3)*(T_im[i,0]+T_im[i,1]+T_im[i,2])) #convert to cross-section
     return spectrum
-
+    
 
 #----function that writes the spectrum to file---#
-def write_to_file(in_file,out_file):
-    
-    spectrum=construct_spectrum(in_file)
-    
+def write_to_file(in_file,out_file,nr_freq,program):
+
+    if program=='dirac':
+        spectrum=construct_spectrum_dirac(in_file,nr_freq)
+    elif program=='dalton':
+        spectrum=construct_spectrum_dalton(in_file)
+          
     for i in range(0,len(spectrum[:,0])):
         out_file.write(str(spectrum[i,0])+ ' ')
         out_file.write(str(spectrum[i,1])+ ' ')
@@ -71,12 +109,12 @@ def write_to_file(in_file,out_file):
 
 
 
-files=['camb3lyp_cpp_1_5AA_HF_3c_opt_5AA_HF_3c_opt.out','camb3lyp_cpp_2_5AA_HF_3c_opt_5AA_HF_3c_opt.out','camb3lyp_cpp_3_5AA_HF_3c_opt_5AA_HF_3c_opt.out','camb3lyp_cpp_4_5AA_HF_3c_opt_5AA_HF_3c_opt.out','camb3lyp_cpp_5_5AA_HF_3c_opt_5AA_HF_3c_opt.out','camb3lyp_cpp_6_5AA_HF_3c_opt_5AA_HF_3c_opt.out']
+files_dalton=['dal_test.out']
 
 f = open('spect.txt', 'w')
 
-for file in files:
-    write_to_file(file,f)
+for file in files_dalton:
+    write_to_file(file,f,3,'dalton')
 
 f.close()
 
